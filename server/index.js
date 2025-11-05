6f5c8b465902597b8167cef194395c29cd6dc1a8
sakina <sakinamemon116@gmail.com>
2025-11-05 20:08:27 +0500

    Updated files and fixed some issues

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use("/images", express.static(path.join(process.cwd(), "images")));
 
 // ✅ Enable CORS FIRST — before routes
 const corsOptions = {
-  origin: ["http://localhost:3001", "https://blog-website-kbge.vercel.app"],
+  origin: ["http://localhost:3001", "https://blog-website-zclq.vercel.app"],
   
   credentials: true,
 };
